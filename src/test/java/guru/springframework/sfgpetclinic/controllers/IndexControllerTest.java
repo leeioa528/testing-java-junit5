65dd6a289f0669b5c6f3597894f8ba08189a3286
hz296742 <lee.loannidis@hertz.com>
2019-10-16 20:34:26 -0500

    Just added comments for the lesson, so i know what happens

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -16,15 +16,17 @@ class IndexControllerTest {
 
     @Test
     void index() {
+        //message  is displayed only if the test case fails
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong View Returned");
-
+        //the labmda  expression only happens when the assertion fails
         assertEquals("index", controller.index(), () -> "Another Expensive Message " +
                 "Make me only if you have to");
     }
 
     @Test
     void oupsHandler() {
+        //the labmda  expression only happens when the assertion fails
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
                 "Message to build" +
                 "for my test");
